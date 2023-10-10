23eb05f70300dad550c62677835710a7f99403df
Lucas Craveiro <lucashcraveiro@gmail.com>
2023-10-10 15:54:21 -0700

    Add left bar inside root layout

--- invoice-FE/src/App.tsx
+++ invoice-FE/src/App.tsx
@@ -1,27 +1,27 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import "./App.css";
+import Root from "./pages/Root";
+import Home from "./pages/Home";
 import ErrorPage from "./pages/Error";
 import InvoiceDetails from "./pages/InvoiceDetails";
-import Home from "./pages/Home";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Home />,
+      element: <Root />,
       errorElement: <ErrorPage />,
-      // children: [
-      //   {
-      //     path: "invoiceDetails",
-      //     element: <InvoiceDetails />,
-      //   },
-      // ],
-    },
-    {
-      path: "/invoiceDetails",
-      element: <InvoiceDetails />,
-      errorElement: <ErrorPage />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "invoiceDetails",
+          element: <InvoiceDetails />,
+        },
+      ],
     },
   ]);
 
