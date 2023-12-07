09f73ab805bfe61129b25f1e5b7f9648a5ed6956
Lucas Craveiro <lucashcraveiro@gmail.com>
2023-12-06 22:24:40 -0800

    Add inputs for Add Invoice Form

--- invoice-FE/src/pages/AddInvoice.tsx
+++ invoice-FE/src/pages/AddInvoice.tsx
@@ -1,17 +1,45 @@
 import MainWrapper from "../components/MainWrapper";
-import InvoiceWrapper from "../components/InvoiceWrapper";
 import GoBack from "../components/base/GoBack";
 import TextInput from "../components/base/TextInput";
+import classes from "./AddInvoice.module.css";
 
 const AddInvoice = () => {
   return (
     <MainWrapper>
-      <InvoiceWrapper>
+      <div className={classes.addWrapper}>
         <GoBack />
 
-        <h1>New Invoice</h1>
-        <TextInput label="Ok" placeholder="Add your name" />
-      </InvoiceWrapper>
+        <h1 className={classes.topHeader}>New Invoice</h1>
+        <div>
+          <h3 className={classes.boxHeader}>Bill From</h3>
+          <TextInput
+            label="Street Address"
+            id={"Street Address"}
+            placeholder="Add your name"
+          />
+          <div className={classes.inputs_wrapper}>
+            <TextInput label="City" placeholder="Add your name" />
+            <TextInput label="Postal Code" placeholder="Add your name" />
+            <TextInput label="Country" placeholder="Add your name" />
+          </div>
+        </div>
+        <div>
+          <h3 className={classes.boxHeader}>Bill To</h3>
+          <TextInput label="Street Address" placeholder="Add your name" />
+          <TextInput label="Street Address" placeholder="Add your name" />
+          <TextInput label="Street Address" placeholder="Add your name" />
+          <div className={classes.inputs_wrapper}>
+            <TextInput label="City" placeholder="Add your name" />
+            <TextInput label="Postal Code" placeholder="Add your name" />
+            <TextInput label="Country" placeholder="Add your name" />
+          </div>
+          <div className={classes.inputs_wrapper}>
+            <TextInput label="City" placeholder="Add your name" />
+            <TextInput label="Postal Code" placeholder="Add your name" />
+          </div>
+          <TextInput label="Street Address" placeholder="Add your name" />
+        </div>
+      </div>
     </MainWrapper>
   );
 };
