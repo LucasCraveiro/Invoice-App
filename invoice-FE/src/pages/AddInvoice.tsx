26edf03783e691af4eb5226d84778c517f3f3e1f
Lucas Craveiro <lucashcraveiro@gmail.com>
2023-12-07 16:34:29 -0800

    Fixes for id and placeholders

--- invoice-FE/src/pages/AddInvoice.tsx
+++ invoice-FE/src/pages/AddInvoice.tsx
@@ -14,30 +14,74 @@ const AddInvoice = () => {
           <h3 className={classes.boxHeader}>Bill From</h3>
           <TextInput
             label="Street Address"
-            id={"Street Address"}
-            placeholder="Add your name"
+            id={"StreetAddress"}
+            placeholder="Add your Street"
           />
           <div className={classes.inputs_wrapper}>
-            <TextInput label="City" placeholder="Add your name" />
-            <TextInput label="Postal Code" placeholder="Add your name" />
-            <TextInput label="Country" placeholder="Add your name" />
+            <TextInput label="City" id={"City"} placeholder="Add your city" />
+            <TextInput
+              label="Postal Code"
+              id={"PostalCode"}
+              placeholder="Add your Postal code"
+            />
+            <TextInput
+              label="Country"
+              id={"Country"}
+              placeholder="Add your country"
+            />
           </div>
         </div>
         <div>
           <h3 className={classes.boxHeader}>Bill To</h3>
-          <TextInput label="Street Address" placeholder="Add your name" />
-          <TextInput label="Street Address" placeholder="Add your name" />
-          <TextInput label="Street Address" placeholder="Add your name" />
+          <TextInput
+            label="Client’s Name"
+            id="clientName"
+            placeholder="Add Client’s Name"
+          />
+          <TextInput
+            label="Client’s Email"
+            id="clientEmail"
+            placeholder="Add client's email"
+          />
+          <TextInput
+            label="Street Address"
+            id="StreetAdressTo"
+            placeholder="Add client's street"
+          />
           <div className={classes.inputs_wrapper}>
-            <TextInput label="City" placeholder="Add your name" />
-            <TextInput label="Postal Code" placeholder="Add your name" />
-            <TextInput label="Country" placeholder="Add your name" />
+            <TextInput
+              label="City"
+              id={"CityTo"}
+              placeholder="Add client's city"
+            />
+            <TextInput
+              label="Postal Code"
+              id={"PostalCodeTo"}
+              placeholder="Add client's Postal code"
+            />
+            <TextInput
+              label="Country"
+              id={"CountryTo"}
+              placeholder="Add client's country"
+            />
           </div>
           <div className={classes.inputs_wrapper}>
-            <TextInput label="City" placeholder="Add your name" />
-            <TextInput label="Postal Code" placeholder="Add your name" />
+            <TextInput
+              label="Invoice Date"
+              id="InvoiceDate"
+              placeholder="Add Invoice Date"
+            />
+            <TextInput
+              label="Payment Terms"
+              id="PaymentTerms"
+              placeholder="Add Payment Terms"
+            />
           </div>
-          <TextInput label="Street Address" placeholder="Add your name" />
+          <TextInput
+            label="Project Description"
+            id="ProjectDescription"
+            placeholder="Add your Project Description"
+          />
         </div>
       </div>
     </MainWrapper>
